6511ca4945809580d6e3925f99897dec85025347
Zack Ulissi <zulissi@meta.com>
2025-05-22 04:22:06 +0000

    default buttons to interactive

--- app.py
+++ app.py
@@ -100,8 +100,8 @@ def main():
     )
     relax_unit_cell = gr.Checkbox(value=False, label="Relax Unit Cell")
 
-    md_button = gr.Button("Run MD Simulation", interactive=False)
-    optimization_button = gr.Button("Run Optimization", interactive=False)
+    md_button = gr.Button("Run MD Simulation", interactive=True)
+    optimization_button = gr.Button("Run Optimization", interactive=True)
 
     output_structure = Molecule3D(
         label="Simulation Visualization",
